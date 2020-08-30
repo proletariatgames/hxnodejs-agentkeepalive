6ccb73e6d20d637acba191ce6446191a7eb0203e
Caue Waneck <waneck@gmail.com>
2020-08-30 08:09:22 -0300

    Fix the HTTPS agent

--- src/js/npm/agentkeepalive/HttpsAgent.hx
+++ src/js/npm/agentkeepalive/HttpsAgent.hx
@@ -1,6 +1,6 @@
 package js.npm.agentkeepalive;
 
-@:jsRequire("agentkeepalive")
+@:jsRequire("agentkeepalive", "HttpsAgent")
 extern class HttpsAgent extends js.node.https.Agent
 {
 	public function new(options:Options);
